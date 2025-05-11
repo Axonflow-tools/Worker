680768a453d7039d24d50bc833f212733e01801f
Ayush-vachhani <ayupatel107@gmail.com>
2025-05-11 13:05:27 +0530

    Updated file service

--- Services/FileService.cs
+++ Services/FileService.cs
@@ -2,11 +2,5 @@ namespace Worker.Services;
 
 public static class FileService
 {
-    private static string? _currentFile { get; set; }
-
-    public static string? CurrentFile
-    {
-        get => _currentFile;
-        set => _currentFile = value;
-    }
+    public static string? CurrentFile { get; set; }
 }
